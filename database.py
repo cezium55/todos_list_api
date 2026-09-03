9c5075bbbf85f968b8cf263e8111edf3857aebcb
Garvit Gaur <128956007+cezium55@users.noreply.github.com>
2026-09-04 02:09:39 +0530

    Link todos to users with owner_id ForeignKey
    Added ForeignKey relationship for todo ownership.

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 #this is for database file creating of todos.db
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
@@ -17,7 +17,6 @@ base = declarative_base()
 
 class UserDB(base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key = True, index=True)
     username = Column(String, unique = True, index = True)
     hashed_password= Column(String)
@@ -29,10 +28,9 @@ class TodoDB(base):
     id = Column(Integer, primary_key=True, index = True)
     title = Column(String, index = True)
     completed = Column(Boolean, default=False)
+    # links each todo to the user who owns it, so users only ever see their own tasks
+    owner_id = Column(Integer, ForeignKey("users.id"), index=True)
 
 #this files looks at the class above and makes the database file in your drive
 base.metadata.create_all(bind=engine)
-
 # we are using sqlalchemy ORM with sqlite because you see firstly we can easily migrate this with one line of code change to postgre sql but sqlaclhemy sqlite is used for desktop cli, or small offline applications that stores database along side of python code so the sqlalchemy is a tool which helps in changing the python code into sqlite format and putting protective layer through the use orm right, and above is the basic boiler plate code of how all this works
-
-
